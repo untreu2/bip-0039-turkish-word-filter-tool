2d3eeb40e200e84159e9ecdc8ad1ac82dfe0affe
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-01-31 20:42:44 +0300

    Update 2-delete_special.py

--- 2-delete_special.py
+++ 2-delete_special.py
@@ -4,7 +4,7 @@ def delete_special(file_path):
         with open(file_path, "r", encoding="utf-8") as file:
             lines = file.readlines()
 
-        new_lines = [line for line in lines if not any(letter in line for letter in "çşüöıwxâêôîûğ.,;!?-:‘¬’)é(–“”")]
+        new_lines = [line for line in lines if not any(letter in line for letter in "çşüöıwxâêôîûğ.,;!?-:‘¬*’)'é(–“”")]
 
        
         with open(file_path, "w", encoding="utf-8") as file:
