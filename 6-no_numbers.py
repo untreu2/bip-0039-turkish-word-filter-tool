114a2114230e19401efaef07e32bbeff31b3be67
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-01-31 20:13:38 +0300

    Update 6-no_numbers.py

--- 6-no_numbers.py
+++ 6-no_numbers.py
@@ -1,9 +1,9 @@
-with open('finals.txt', 'r') as file:
+with open('finals.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
 
 filtered_lines = [line for line in lines if not any(char.isdigit() for char in line)]
 
-with open('finals.txt', 'w') as file:
+with open('finals.txt', 'w', encoding='utf-8') as file:
     file.writelines(filtered_lines)
 
 print("Deleted.")
