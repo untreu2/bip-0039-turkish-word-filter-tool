d4e1cb5fed87f735e98a4cd56468a953e207a75b
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-01-31 20:13:03 +0300

    Update 4-four_letters_control.py

--- 4-four_letters_control.py
+++ 4-four_letters_control.py
@@ -1,10 +1,10 @@
 def remove_short_words_lines(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
     new_lines = [line for line in lines if not shortest_word_length(line) < 4]
 
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(new_lines)
 
 def shortest_word_length(line):
