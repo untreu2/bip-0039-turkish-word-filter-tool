23ce8f2084437b3dc1f4103398ba157a49eaf6bf
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-02-01 00:59:13 +0300

    Update 0-main.py

--- 0-main.py
+++ 0-main.py
@@ -4,9 +4,12 @@ subprocess.run(["python", "1-make_list.py"])
 subprocess.run(["python", "2-delete_special.py"])
 subprocess.run(["python", "3-different_letters.py"])
 subprocess.run(["python", "4-four_letters_control.py"])
+subprocess.run(["python", "4.1-first_four_letters.py"])
 subprocess.run(["python", "5-possessive&verb_control.py"])
 subprocess.run(["python", "6-no_numbers.py"])
 subprocess.run(["python", "7-no_capital.py"])
 subprocess.run(["python", "8-compare_with_others.py"])
+
+
 
 #Sadece bu dosyayı çalıştırın.
