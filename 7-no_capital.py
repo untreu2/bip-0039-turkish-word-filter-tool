1812493775614c9ed2b6819fa433438dcac9e2d2
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-01-31 19:28:19 +0300

    Update 7-no_capital.py

--- 7-no_capital.py
+++ 7-no_capital.py
@@ -1,10 +1,8 @@
 with open('finals.txt', 'r') as file:
     lines = file.readlines()
 
-# Büyük harf içeren satırları filtrele
 filtered_lines = [line for line in lines if not any(char.isupper() for char in line)]
 
-# Dosyayı tekrar yaz
 with open('finals.txt', 'w') as file:
     file.writelines(filtered_lines)
 
