f7dd8de918edd68c274ae09f373cb4806ebda368
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-02-01 01:38:50 +0300

    Update 2-delete_special.py

--- 2-delete_special.py
+++ 2-delete_special.py
@@ -1,17 +1,20 @@
-def delete_special(file_path):
-    
-    
-        with open(file_path, "r", encoding="utf-8") as file:
-            lines = file.readlines()
+import re
 
-        new_lines = [line for line in lines if not any(letter in line for letter in "çşüöıwxâêôîûğ.•,;!?·-«»^æ:‘¬*’—)'é\"(–“”")]
+def clean_and_overwrite_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-       
-        with open(file_path, "w", encoding="utf-8") as file:
-            file.writelines(new_lines)
+    cleaned_lines = []
 
-        print("Deleted.")
+    for line in lines:
+        # Remove lines containing non-English alphabet characters and 'x', 'w', 'q'
+        if re.search('[^a-zA-Z\s]', line) or any(char in line for char in ['x', 'w', 'q']):
+            continue
+        cleaned_lines.append(line)
 
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(cleaned_lines)
 
-file_path = "finals.txt"  
-delete_special(file_path)
+# Example usage
+file_path = 'finals.txt'
+clean_and_overwrite_file(file_path)
