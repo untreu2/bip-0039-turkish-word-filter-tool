1db6dc9445c2553c4eb16446ddce5b43cae836d0
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-02-01 01:15:55 +0300

    Update 5-possessive&verb_control.py

--- 5-possessive&verb_control.py
+++ 5-possessive&verb_control.py
@@ -4,25 +4,26 @@ with open("finals.txt", "r", encoding="utf-8") as file:
 cleaned_words = []
 for word in words:
     word = word.strip() 
-    if not word.endswith(( "-lik", "-lık", "-luk", "-lük",
-    "-ci", "-cı", "-cu", "-cü",
-    "-ken",
-    "-ce",
-    "-er", "-ar", "-ır", "-ir", "-ur", "-ür",
-    "-mek", "-mak",
-    "-iş", "-ış", "-üş", "-ış",
-    "-dir", "-tir",
-    "-li", "-lı", "-lu", "-lü",
-    "-siz", "-sız", "-suz", "-süz",
-    "-msi", "-imsi",
-    "-ncı", "-ncu", "-nçi", "-nçu",
-    "-ce", "-ca", "-ça",
-    "-ken",
-    "-de", "-da", "-te", "-ta",
-    "-me", "-ma",
-    "-di", "-ti", "-dı", "-tı",
-    "-de", "-da", "-te", "-ta",
-    "-den", "-dan", "-ten", "-tan")):
+    if not word.endswith(( "lik", "lık", "luk", "lük",
+    "ci", "cı", "cu", "cü",
+    "ken",
+    "ce",
+    "er", "ar", "ır", "ir", "ur", "ür",
+    "mek", "mak",
+    "iş", "ış", "üş", "ış",
+    "dir", "tir",
+    "li", "lı", "lu", "lü",
+    "siz", "sız", "suz", "süz",
+    "msi", "imsi",
+    "ncı", "ncu", "nçi", "nçu",
+    "ce", "ca", "ça",
+    "ken",
+    "de", "da", "te", "ta",
+    "me", "ma",
+    "di", "ti", "dı", "tı",
+    "de", "da", "te", "ta",
+    "den", "dan", "ten", "tan","erek","arak", "im","ım","üm,","in","ın","un","ün","i","ı","u","ü",
+    "imiz","ımız","umuz","ümüz","iniz","ınız","unuz","ünüz","an","en","le","la","dum","dim","din","dün","dan","den","de","da")):
         cleaned_words.append(word)
 
 with open("finals.txt", "w", encoding="utf-8") as file:
