ed83e1a9d2e2ca9f58ce789c35e4c02d0965447b
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-01-31 19:41:35 +0300

    Update 5-possessive&verb_control.py

--- 5-possessive&verb_control.py
+++ 5-possessive&verb_control.py
@@ -4,7 +4,7 @@ with open("finals.txt", "r", encoding="utf-8") as file:
 cleaned_words = []
 for word in words:
     word = word.strip() 
-    if not word.endswith(("m", "n", "i", "miz", "niz", "leri", "ler", "lar","de","da","ken","suna","sine","sına","süne")):
+    if not word.endswith(("m", "n", "i", "miz", "niz", "leri", "ler", "lar","de","da","ken","suna","sine","sına","süne","ine","ına")):
         cleaned_words.append(word)
 
 with open("finals.txt", "w", encoding="utf-8") as file:
