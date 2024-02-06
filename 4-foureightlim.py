e3ca89e9a16b5154b3c630f8350daf7ff689479a
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-02-06 23:27:36 +0300

    Update 4-foureightlim.py

--- 4-foureightlim.py
+++ 4-foureightlim.py
@@ -1,14 +1,7 @@
-def remove_short_words_lines(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
+with open('finals.txt', 'r', encoding='utf-8') as file:
+    lines = file.readlines()
 
-    new_lines = [line for line in lines if not any(len(word) < 4 or len(word) > 8 for word in line.split())]
+filtered_lines = [line for line in lines if len(line.strip()) <= 8 and len(line.strip()) >= 4]
 
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.writelines(new_lines)
-
-file_path = "finals.txt"
-
-remove_short_words_lines(file_path)
-
-print("Deleted.")
+with open('finals.txt', 'w', encoding='utf-8') as file:
+    file.writelines(filtered_lines)
