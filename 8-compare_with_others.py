747293424c78371f3922aa4c2d620098d5554118
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-02-13 00:53:22 +0300

    Update 8-compare_with_others.py

--- 8-compare_with_others.py
+++ 8-compare_with_others.py
@@ -7,7 +7,7 @@ with open(finals_file_path, "r", encoding="utf-8") as finals_file:
 with open(allseed_file_path, "r", encoding="utf-8") as allseed_file:
     allseed_lines = allseed_file.readlines()
 
-filtered_finals_lines = [line for line in finals_lines if not any(line.startswith(seed[:4]) for seed in allseed_lines)]
+filtered_finals_lines = [line for line in finals_lines if not any(line.startswith(seed[:3]) for seed in allseed_lines)]
 
 with open(finals_file_path, "w", encoding="utf-8") as updated_finals_file:
     updated_finals_file.writelines(filtered_finals_lines)
