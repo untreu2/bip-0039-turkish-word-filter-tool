6243616e5434a8c11ae3b643371aea696c767ef7
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-02-13 02:20:26 +0300

    Update 4.1-first_three_letters.py

--- 4.1-first_three_letters.py
+++ 4.1-first_three_letters.py
@@ -5,7 +5,7 @@ unique_lines = []
 seen_prefixes = set()
 
 for line in lines:
-    prefix = line[:4]
+    prefix = line[:3]
     if prefix not in seen_prefixes:
         unique_lines.append(line)
         seen_prefixes.add(prefix)
