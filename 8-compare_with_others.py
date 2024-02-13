97f12034e38dc450157787416b1ad0aae0116f4d
emir yorulmaz <emiryorulmaz@ikmail.com>
2024-02-13 04:08:15 +0300

    Update 8-compare_with_others.py

--- 8-compare_with_others.py
+++ 8-compare_with_others.py
@@ -1,17 +1,34 @@
-with open("finals.txt", "r", encoding="utf-8") as finals:
-    finals_lines = finals.readlines()
+with open('finals.txt', 'r' , encoding='utf-8') as finals_file:
+    finals_lines = finals_file.readlines()
 
-with open('allseed.txt', 'r', encoding="utf-8") as allseed:
-    allseed_lines = allseed.readlines()
+with open('allseed.txt', 'r', encoding='utf-8') as allseed_file:
+    allseed_lines = allseed_file.readlines()
 
-match = set(line[:4] for line in finals_lines).intersection(line[:4] for line in allseed_lines)
-
-filtered = []
+newf = []
 for line in finals_lines:
-    if line[:4] not in match and line not in allseed_lines:
-        filtered.append(line)
+    line = line.strip()
+    keep_line = True
 
-with open('finals.txt', 'w') as finals:
-    finals.writelines(filtered)
+    for allseed_line in allseed_lines:
+        allseed_line = allseed_line.strip()
 
-print("Process completed.")
+        if line == allseed_line:
+            keep_line = False
+            break
+
+        if line[:4] == allseed_line[:4]:
+            keep_line = False
+            break
+
+        if len(line) == 3 and len(allseed_line) >= 4 and line == allseed_line[:3]:
+            keep_line = True
+            break
+
+        if len(allseed_line) < 3:
+            continue
+
+    if keep_line:
+        newf.append(line)
+
+with open('finals.txt', 'w') as finals_file:
+    finals_file.write('\n'.join(newf))
